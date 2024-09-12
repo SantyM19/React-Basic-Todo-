7428e374e27614c088d9ff94f055997c037d5103
Santy <santiagomejiad19@gmail.com>
2024-09-12 15:53:38 -0500

    events added

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./CreateTodoButton.css";
 
 function CreateTodoButton() {
-  return <button className="CreateTodoButton">+</button>;
+  return <button 
+  onClick={()=>console.log("Click ñeri")}
+  className="CreateTodoButton">+</button>;
 }
 
 export { CreateTodoButton };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,9 +2,20 @@ import React from 'react'
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
-    <input className='TodoSearch' placeholder="Buscar..." />
-  )
+    <input
+      placeholder="conquistar el mundo"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
