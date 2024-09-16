631bf55004b9f043f4dcc27ef38522cf9a6598dd
Santy <santiagomejiad19@gmail.com>
2024-09-15 23:27:26 -0500

    Add Todo added

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -2,19 +2,24 @@ import React from "react";
 import { TodoContext } from "../TodoContext";
 import "./TodoForm.css";
 function TodoForm() {
+  
   const { addTodo, setOpenModal } = React.useContext(TodoContext);
   const [newTodoValue, setNewTodoValue] = React.useState("");
+
   const onSubmit = (event) => {
     event.preventDefault();
     addTodo(newTodoValue);
     setOpenModal(false);
   };
+
   const onCancel = () => {
     setOpenModal(false);
   };
+
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
   };
+
   return (
     <form onSubmit={onSubmit}>
       <label>Esribe tu nuevo TODO</label>
